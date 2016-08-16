05882343f0242e4d818a91d5b1aaca02506f6e88
pankaj <pankaj.pundit001@gmail.com>
2016-08-16 08:52:04 +0530

    added some router functionality

--- BasicRouter.js
+++ BasicRouter.js
@@ -1,9 +1,36 @@
 'use strict';
 const http = require('http');
+const url = require('url');
+
+let routes = {
+    'GET' : {
+        '/' : (req, res) => {
+                res.writeHead(200, {'content-type':'text/html'});
+                res.end('<h1>Basic Router!</h1>');
+        }, 
+            '/about' : (req, res) => {
+                res.writeHead(200, {'content-type' : 'text/html'});
+                res.end('<h1>About Page</h1>');
+            }
+        },
+    'POST' : {
+
+    },
+    'NA' : (req, res) => {
+            res.writeHead(404);
+        res.end('content not found!');
+    }
+}
 
 function router(req, res){
-    res.writeHead(200, {'content-type':'html'});
-    res.end('<h1>Basic Router!</h1>')
+    let baseUri = url.parse(req.url, true);
+    console.log('baseUri : ',baseUri);
+    let resolveRoute = routes[req.method][baseUri.pathname];
+    if(resolveRoute!=undefined){
+        resolveRoute(req, res);
+    } else {
+        routes['NA'](req,res);
+    }
 }
 
 http.createServer(router)
